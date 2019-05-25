d181fd1154877524c8076a61f42d1e8143707e10
Mazino Ukah <ewomaukah@yahoo.com>
2019-05-25 11:41:43 +0100

    Change less file name for innerNav component

--- components/shared/InnerNav.js
+++ components/shared/InnerNav.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Menu, Sticky } from 'semantic-ui-react'
 import _ from 'lodash'
 
-import '../shared/less/innerNav.less';
+import './less/nav.less';
 
 
 export default class InnerNav extends Component {
