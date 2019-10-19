a4a0fb06ead22fc59c216c40e29dd03216c3cb5d
it_unit <it_unit@cardinalstone.com>
2019-10-19 20:53:24 +0100

    fix crashing categories link

--- components/serviceProviders/providersForm.js
+++ components/serviceProviders/providersForm.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { Grid, Select, Input } from 'semantic-ui-react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { Grid, Select, Input } from 'semantic-ui-react'
+import { connect } from 'react-redux'
 
-import { Range } from 'rc-slider';
-import 'rc-slider/assets/index.css';
-import 'rc-tooltip/assets/bootstrap.css';
-import * as actions from '../../store/actions';
+import { Range } from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import 'rc-tooltip/assets/bootstrap.css'
+import * as actions from '../../store/actions'
 
-import { DatePicker } from 'antd';
-import "./less/providerForm.less"
+import { DatePicker } from 'antd'
+import './less/providerForm.less'
 import { getAllServices } from '../../services/generatData.ts'
 import dayjs from 'dayjs'
-import Router from 'next/router';
+import Router from 'next/router'
 
 const options = [
   { key: '', text: 'Not Applicable', value: '' },
@@ -24,13 +24,17 @@ class ProviderForm extends Component {
     value: { min: 2, max: 10 },
     styles: {},
     value: { start: 20, end: 80 },
-    selectedValues: {min: 20, max: 100},
+    selectedValues: { min: 20, max: 100 },
     options: [],
     distance: [
-      { key: 'less than 1 mile', text: 'less than 1 mile', value: 'less than 1 mile' },
+      {
+        key: 'less than 1 mile',
+        text: 'less than 1 mile',
+        value: 'less than 1 mile',
+      },
       { key: '1 - 5 miles', text: '1 - 5 miles', value: '1 - 5 miles' },
       { key: '6 - 10 miles', text: '6 - 10 miles', value: '6 - 10 miles' },
-      { key: 'Over 10 miles', text: 'Over 10 miles', value: 'Over 10 miles' }
+      { key: 'Over 10 miles', text: 'Over 10 miles', value: 'Over 10 miles' },
     ],
     formFields: {
       sortBy: '',
@@ -38,189 +42,231 @@ class ProviderForm extends Component {
       postcode: '',
       distance: '',
       // when: null,
-      priceRange: ''
+      priceRange: '',
     },
     services: [],
     when: null,
   }
 
-
   updateForm_ = (a, b) => {
-    this.setState({when: a}, () => {
-      let day = dayjs(b).$d.toString().split(' ')[0]
+    this.setState({ when: a }, () => {
+      let day = dayjs(b)
+        .$d.toString()
+        .split(' ')[0]
       this.props.getWhenServiceNeeded(day)
     })
   }
-  
-  onChange = (event) => {
+
+  onChange = event => {
     this.setState(
       {
         selectedValues: {
-        min: event[0],
-        max: event[1]
-      }
-    }, () => {
-      let fields = {...this.state.formFields}
-      fields.priceRange = `${this.state.selectedValues.min} - ${this.state.selectedValues.max}`
-      this.setState({formFields: fields})
-    })
+          min: event[0],
+          max: event[1],
+        },
+      },
+      () => {
+        let fields = { ...this.state.formFields }
+        fields.priceRange = `${this.state.selectedValues.min} - ${this.state.selectedValues.max}`
+        this.setState({ formFields: fields })
+      },
+    )
   }
 
   updateForm = (e, data, type) => {
-    let fields = {...this.state.formFields}
-    e.target.value ? fields[type] = e.target.value : fields[type] = data.value
-    this.setState({formFields: fields}, () => {
+    let fields = { ...this.state.formFields }
+    e.target.value
+      ? (fields[type] = e.target.value)
+      : (fields[type] = data.value)
+    this.setState({ formFields: fields }, () => {
       this.props.getFormData(this.state.formFields)
     })
   }
 
   clearFilter = () => {
-    this.setState({formFields: {
-        sortBy: '',
-        searchFor: '',
-        postcode: '',
-        distance: '',
-        priceRange: ''
-      }
-    }, () => {
-      this.props.getFormData(this.state.formFields)
-    })
+    this.setState(
+      {
+        formFields: {
+          sortBy: '',
+          searchFor: '',
+          postcode: '',
+          distance: '',
+          priceRange: '',
+        },
+      },
+      () => {
+        this.props.getFormData(this.state.formFields)
+      },
+    )
   }
-  
-  
-  componentDidMount () {
+
+  componentDidMount() {
     let query = Router.router.query
-    Object.keys(query).length > 0 ? this.setState({formFields: query}) : null
+    Object.keys(query).length > 0 ? this.setState({ formFields: query }) : null
     getAllServices()
-    .then(res => {
-      this.props.saveServices(res.data.data)
-      this.setState({
-        services: res.data.data
-      }, () => {
-        let options = []
-        Object.keys(this.state.services).map(key => {
-          options.push(this.state.services[key])
-        })
-        this.setState({options: options.map(option => {
-          return {
+      .then(res => {
+        this.props.saveServices(res.data.data)
+        this.setState(
+          {
+            services: res.data.data,
+          },
+          () => {
+            let options = []
+            Object.keys(this.state.services).map(key => {
+              options.push(this.state.services[key])
+            })
+            this.setState({
+              options: options.map(option => {
+                return {
                   text: option.serviceName,
                   key: option._id,
-                  value: option.serviceName  
+                  value: option.serviceName,
                 }
-          })
-        })
+              }),
+            })
+          },
+        )
       })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   // componentDidUpdate() {
   // }
 
-  render () {
+  render() {
     return (
       <>
-      <div className="providerForm pageWrap">
-        <Grid columns={3} stackable>
+        <div className="providerForm pageWrap">
+          <Grid columns={3} stackable>
             <Grid.Row>
-                <Grid.Column  className="RangeCol">
-                    <Select
-                    className="Select"
-                    onChange={(e, data) => this.updateForm(e, data, 'sortBy')}
-                    value={this.state.formFields.sortBy}
-                    fluid
-                    options={this.state.options}
+              <Grid.Column className="RangeCol">
+                <Select
+                  className="Select"
+                  onChange={(e, data) => this.updateForm(e, data, 'sortBy')}
+                  value={this.state.formFields.sortBy}
+                  fluid
+                  options={this.state.options}
+                />
+                <img
+                  src="../../static/icons/sort.svg"
+                  className="selectIcon"
+                  alt=""
+                />
+                <span className="selectLabel">Sort by</span>
+              </Grid.Column>
+              <Grid.Column className="RangeCol">
+                <Input
+                  type="text"
+                  className="FormInput"
+                  onChange={(e, data) => this.updateForm(e, data, 'searchFor')}
+                  value={this.state.formFields.searchFor}
+                  size="huge"
+                  placeholder="Search term goes here"
+                  fluid
+                />
+                <img
+                  src="../../static/icons/search.svg"
+                  className="inputIcon"
+                  alt=""
+                />
+                <span className="myLabel">Search for</span>
+              </Grid.Column>
+              <Grid.Column className="RangeCol">
+                <Input
+                  type="text"
+                  className="FormInput"
+                  size="huge"
+                  onChange={(e, data) => this.updateForm(e, data, 'postcode')}
+                  value={this.state.formFields.postcode}
+                  placeholder="WC1A 1AA"
+                  fluid
+                />
+                <img
+                  src="../../static/icons/mile.svg"
+                  className="inputIcon"
+                  alt=""
+                />
+                <span className="myLabel">Post code</span>
+              </Grid.Column>
+              <Grid.Column className="RangeCol">
+                <Select
+                  className="Select"
+                  onChange={(e, data) => this.updateForm(e, data, 'distance')}
+                  value={this.state.formFields.distance}
+                  fluid
+                  options={this.state.distance}
+                />
+                <img
+                  src="../../static/icons/mile.svg"
+                  className="selectIcon"
+                  alt=""
+                />
+                <span className="selectLabel">Distance</span>
+              </Grid.Column>
+              <Grid.Column className="RangeCol">
+                <DatePicker
+                  className="date--picker"
+                  onChange={(e, data) => this.updateForm_(e, data)}
+                  value={this.state.when}
+                  showTime
+                  placeholder="When do you want this?"
+                  suffixIcon={
+                    <img
+                      className="PickerIcon"
+                      src="../../static/images/calender.png"
                     />
-                    <img src="../../static/icons/sort.svg" className="selectIcon" alt=""/>
-                    <span className="selectLabel">
-                        Sort by
-                    </span>
-                </Grid.Column>
-                <Grid.Column  className="RangeCol">
-                    <Input
-                    type="text"
-                    className="FormInput"
-                    onChange={(e, data) => this.updateForm(e, data, 'searchFor')}
-                    value={this.state.formFields.searchFor}
-                    size="huge"
-                    placeholder='Search term goes here'
-                    fluid
-                    />
-                    <img src="../../static/icons/search.svg" className="inputIcon" alt=""/>
-                    <span className="myLabel">
-                        Search for
-                    </span>
-                </Grid.Column>
-                <Grid.Column  className="RangeCol">
-                    <Input
-                    type="text"
-                    className="FormInput"
-                    size="huge"
-                    onChange={(e, data) => this.updateForm(e, data, 'postcode')}
-                    value={this.state.formFields.postcode}
-                    placeholder='WC1A 1AA'
-                    fluid
-                    />
-                    <img src="../../static/icons/mile.svg" className="inputIcon" alt=""/>
-                    <span className="myLabel">
-                        Post code
-                    </span>
-                </Grid.Column>
-                <Grid.Column  className="RangeCol">
-                    <Select
-                    className="Select"
-                    onChange={(e, data) => this.updateForm(e, data, 'distance')}
-                    value={this.state.formFields.distance}
-                    fluid
-                    options={this.state.distance}
-                    />
-                    <img src="../../static/icons/mile.svg" className="selectIcon" alt=""/>
-                    <span className="selectLabel">
-                      Distance
-                    </span>
-                </Grid.Column>
-                <Grid.Column  className="RangeCol">
-                    <DatePicker
-                      className="date--picker"
-                      onChange={(e, data) => this.updateForm_(e, data)}
-                      value={this.state.when}
-                      showTime
-                      placeholder="When do you want this?"
-                      suffixIcon={<img className="PickerIcon" src="../../static/images/calender.png" />}
-                    />
-                    <span className="dateLabel">
-                      When do you want this?
-                    </span>
-                </Grid.Column>
-                <Grid.Column className="RangeCol">
-                  <div className="RangeCol_">
-                    <span className="RangeLabel">Price range</span>
-                    <span className="RangeValues">£{this.state.selectedValues.min} - £{this.state.selectedValues.max}</span>
-                      
-                    <Range min={20} max={300} className="slider_" defaultValue={[this.state.selectedValues.min, this.state.selectedValues.max]} onChange={(event) => this.onChange(event)}  />
-                  </div>
-                </Grid.Column>
+                  }
+                />
+                <span className="dateLabel">When do you want this?</span>
+              </Grid.Column>
+              <Grid.Column className="RangeCol">
+                <div className="RangeCol_">
+                  <span className="RangeLabel">Price range</span>
+                  <span className="RangeValues">
+                    £{this.state.selectedValues.min} - £
+                    {this.state.selectedValues.max}
+                  </span>
+
+                  <Range
+                    min={20}
+                    max={300}
+                    className="slider_"
+                    defaultValue={[
+                      this.state.selectedValues.min,
+                      this.state.selectedValues.max,
+                    ]}
+                    onChange={event => this.onChange(event)}
+                  />
+                </div>
+              </Grid.Column>
             </Grid.Row>
-        </Grid>
-      </div>
-        <p style={{
-                  color: '#212B36',
-                  width: '100%',
-                  textAlign: 'right',
-                  fontWeight: 600,
-                  cursor: 'pointer'
-                  }} onClick={this.clearFilter}>Clear filter</p>
+          </Grid>
+        </div>
+        <p
+          style={{
+            color: '#212B36',
+            width: '100%',
+            textAlign: 'right',
+            fontWeight: 600,
+            cursor: 'pointer',
+          }}
+          onClick={this.clearFilter}
+        >
+          Clear filter
+        </p>
       </>
-    );
+    )
   }
 }
 
-
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   services: state.service.beautyServices.allServices,
-  serviceProviders: state.serviceProviders.allProviders
+  serviceProviders: state.serviceProviders.allProviders,
 })
 
-export default  connect(mapStateToProps, actions)(ProviderForm)
+export default connect(
+  mapStateToProps,
+  actions,
+)(ProviderForm)
--- pages/serviceproviders.js
+++ pages/serviceproviders.js
@@ -1,145 +1,170 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import Provider from '../components/serviceProviders/Provider'
 import ProvidersForm from '../components/serviceProviders/providersForm'
 import InnerNav from '../components/shared/InnerNav'
-import Footer from '../components/shared/Footer';
-import withMasterLayout from '../pages/layouts/withMasterLayout';
-import { Container } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { Grid } from 'semantic-ui-react';
+import Footer from '../components/shared/Footer'
+import withMasterLayout from '../pages/layouts/withMasterLayout'
+import { Container } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { Grid } from 'semantic-ui-react'
 import { getProviders } from '../store'
 import './less/serviceProvider.less'
 import * as actions from '../store/actions'
-import Display from '../components/shared/Display';
-import { getAllServices, getAllTrends, getAllProviders } from '../services/generatData.ts'
-import Router from 'next/router';
-import { Button } from 'antd';
-
+import Display from '../components/shared/Display'
+import {
+  getAllServices,
+  getAllTrends,
+  getAllProviders,
+} from '../services/generatData.ts'
+import Router from 'next/router'
+import { Button } from 'antd'
 
 const styles = {
-    pageWrap: {
-    paddingTop: '10px'
-    }
+  pageWrap: {
+    paddingTop: '10px',
+  },
 }
-
-
-
 
 class ServiceProvider extends Component {
-    static async getInitialProps ({ reduxStore, req }) {
-        this.test = reduxStore
-      // DISPATCH ACTIONS HERE ONLY WITH `reduxStore.dispatch`
-  
-      return {
-      }
+  static async getInitialProps({ reduxStore, req }) {
+    this.test = reduxStore
+    // DISPATCH ACTIONS HERE ONLY WITH `reduxStore.dispatch`
+
+    return {}
+  }
+
+  state = {
+    position: '',
+    options: [],
+    allProviders: [],
+  }
+
+  getFormData = (e, f) => {
+    // filter list of providers based on user preferences
+    let newArray = []
+    !f
+      ? (newArray = this.props.serviceProviders.filter(provider => {
+          return (
+            provider.description
+              .toLowerCase()
+              .includes(e.searchFor.toLowerCase()) &&
+            provider.mileRadius
+              .toLowerCase()
+              .includes(e.distance.toLowerCase()) &&
+            provider.postcode.toLowerCase().includes(e.postcode.toLowerCase())
+          )
+        }))
+      : (newArray = this.state.allProviders.filter(provider => {
+          return (
+            provider.description
+              .toLowerCase()
+              .includes(f.searchFor.toLowerCase()) &&
+            provider.postcode
+              .toLowerCase()
+              .includes(f.postcode.toLowerCase()) &&
+            provider.schedules.filter(
+              schedule => schedule.day.toLowerCase() === f.when.toLowerCase(),
+            ).length > 0
+          )
+        }))
+    this.setState({ allProviders: newArray })
+  }
+
+  getWhenServiceNeeded = day => {
+    // let providers = [...this.state.allProviders]
+    let availableProviders = this.props.serviceProviders.filter(prv => {
+      return JSON.stringify(prv.schedules).includes(day)
+    })
+    if (day !== 'Invalid') {
+      this.setState({ allProviders: availableProviders })
+    }
+  }
+
+  componentDidMount() {
+    // console.log(window.localStorage.getItem('store'))
+    let token = window.sessionStorage.getItem('glamourToken')
+    if (token) {
+      //   this.props.saveUserData(JSON.parse(userData))
     }
 
-    state = {
-        position: '',
-        options: [],
-        allProviders: []
-    }
+    // get list of serviceProviders
+    getAllProviders()
+      .then(res => {
+        this.props.saveProviders(res.data.users)
+        Object.keys(Router.router.query).length > 0
+          ? this.setState({ allProviders: res.data.users }, () => {
+              this.getFormData(null, Router.router.query)
+            })
+          : this.setState({ allProviders: res.data.users })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
-    getFormData = (e, f) => {
-        // filter list of providers based on user preferences
-        let newArray = []
-        !f ? newArray = this.props.serviceProviders.filter(provider => {
-            return provider.description.toLowerCase().includes(e.searchFor.toLowerCase())
-                && provider.mileRadius.toLowerCase().includes(e.distance.toLowerCase())
-                && provider.postcode.toLowerCase().includes(e.postcode.toLowerCase())
-        }) : newArray = this.state.allProviders.filter(provider => {
-            return provider.description.toLowerCase().includes(f.searchFor.toLowerCase())
-                && provider.postcode.toLowerCase().includes(f.postcode.toLowerCase())
-                && provider.schedules.filter(schedule => schedule.day.toLowerCase() === f.when.toLowerCase()).length > 0
-        })
-        this.setState({allProviders: newArray})
-    }
+  render() {
+    return (
+      <>
+        <Display if={this.props.isLoggedIn}>
+          <InnerNav userRole={this.props.user.role} />
+        </Display>
+        <Container>
+          <div style={styles.pageWrap} className="serviceProviders">
+            <ProvidersForm
+              getWhenServiceNeeded={this.getWhenServiceNeeded}
+              getFormData={this.getFormData}
+            />
 
-    getWhenServiceNeeded = (day) => {
-        // let providers = [...this.state.allProviders]
-        let availableProviders = this.props.serviceProviders.filter(prv => {
-          return JSON.stringify(prv.schedules).includes(day)
-        })
-        if (day !== 'Invalid') {
-            this.setState({allProviders: availableProviders})
-        }
-    }
-
-    componentDidMount() {
-        // console.log(window.localStorage.getItem('store'))
-        let token = window.sessionStorage.getItem('glamourToken')
-        if (token) {
-        //   this.props.saveUserData(JSON.parse(userData))
-        }
-
-        // get list of serviceProviders    
-        getAllProviders()
-        .then(res => {
-          this.props.saveProviders(res.data.users)
-          Object.keys(Router.router.query).length > 0 ? this.setState({allProviders: res.data.users}, () => {
-            this.getFormData(null, Router.router.query)
-          }) : this.setState({allProviders: res.data.users})
-        })
-        .catch(err => {
-          console.log(err)
-        })
-    }
-
-    render () {
-        return (
-            <>
-            <Display if={this.props.isLoggedIn}>
-                <InnerNav userRole={this.props.user.role} />
-            </Display>
-            <Container>
-                <div style={styles.pageWrap} className="serviceProviders">
-                    <ProvidersForm getWhenServiceNeeded={this.getWhenServiceNeeded} getFormData={this.getFormData} />
-
-                    {
-                        this.state.allProviders.length > 0 ? <Grid columns={3} stackable>
-                        <Grid.Row>
-                            {
-                                this.state.allProviders.map((provider, i) => (
-                                    <Grid.Column key={`provider${i}`}>
-                                        <Provider  {...provider} />
-                                    </Grid.Column>
-                                ))
-                            }
-                        </Grid.Row> 
-                    </Grid>  :
-                    <div className="emptyProviders">
-                        <img src="/static/icons/empty_service.svg" alt=""/>
-                        {
-                            this.props.serviceProviders.length === 0 ? <div>
-                                <h1>
-                                    No Providers yet. Kindly check back later
-                                </h1>
-                            </div> :
-                            <div>
-                                <h1>
-                                    No providers match your specified criteria
-                                </h1>
-                                <Button className="mainBtn secondaryBtn" onClick={() => this.setState({allProviders: this.props.serviceProviders})}>
-                                    View All
-                                </Button>
-                            </div>
-                        }
-                    </div>
-                    }
-                </div>
-            </Container>
-            <Footer />
-            </>
-        );
-    }
+            {this.state.allProviders.length > 0 ? (
+              <Grid columns={3} stackable>
+                <Grid.Row>
+                  {this.state.allProviders.map((provider, i) => (
+                    <Grid.Column key={`provider${i}`}>
+                      <Provider {...provider} />
+                    </Grid.Column>
+                  ))}
+                </Grid.Row>
+              </Grid>
+            ) : (
+              <div className="emptyProviders">
+                <img src="/static/icons/empty_service.svg" alt="" />
+                {this.props.serviceProviders.length === 0 ? (
+                  <div>
+                    <h1>No Providers yet. Kindly check back later</h1>
+                  </div>
+                ) : (
+                  <div>
+                    <h1>No providers match your specified criteria</h1>
+                    <Button
+                      className="mainBtn secondaryBtn"
+                      onClick={() =>
+                        this.setState({
+                          allProviders: this.props.serviceProviders,
+                        })
+                      }
+                    >
+                      View All
+                    </Button>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        </Container>
+        <Footer />
+      </>
+    )
+  }
 }
 
-const mapStateToProps = (state) => ({
-    serviceProviders: getProviders(state),
-    services: state.service.beautyServices.allServices,
-    isLoggedIn: state.user.isLoggedIn,
-    user: state.user
+const mapStateToProps = state => ({
+  serviceProviders: getProviders(state),
+  services: state.service.beautyServices.allServices,
+  isLoggedIn: state.user.isLoggedIn,
+  user: state.user,
 })
 
-
-export default connect(mapStateToProps, actions)(withMasterLayout(ServiceProvider));
+export default connect(
+  mapStateToProps,
+  actions,
+)(withMasterLayout(ServiceProvider))
